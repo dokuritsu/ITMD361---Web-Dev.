877fadf00ff84e4989b981b265b7396837ba6946
Laura Pereda <lypereda17@gmail.com>
2020-04-21 14:40:31 -0500

    completed the javascript portion

--- labs/lab7/js/script.js
+++ labs/lab7/js/script.js
@@ -1,11 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
- 
 
+    var button = document.getElementById("entrybutton");
+    var msg = document.getElementById("entryinput");
+    var output = document.getElementById("textoutput");
 
+    button.addEventListener("click", function(){
+        output.innerHTML = msg.value;
+        alert("Laura: " + msg.value);
+    });
 
-
-
-
-
+}
 window.addEventListener('load', init);
