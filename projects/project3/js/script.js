0d5ec4f6902a56d416dc7c5b781ce5b704f415e6
Laura Pereda <lypereda17@gmail.com>
2020-04-21 17:02:18 -0500

    working map; still shows dev mode..

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -3,7 +3,8 @@ function initMap() {
     var loc = {lat: -34.397, lng: 150.644};
 
     // Map
-    var map = new google.map.Map(document.getElementById('map'), {zoom: 4, center: loc});
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: loc});
 
     // Marker
     var marker = new google.maps.Marker({position: loc, map: map});
